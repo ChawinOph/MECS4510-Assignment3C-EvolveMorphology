2991ea773afc3ea9fd2b18b65f2267b60bcbbfe4
Rosie <rcm2146@columbia.edu>
2018-12-07 20:01:36 -0500

    Started the main_evolution script

--- crossoverMat.m
+++ crossoverMat.m
@@ -6,40 +6,49 @@ cut = [randperm(5,2); randperm(9,2)]
 if cut(1,1) < cut(1,2)
     if cut(2,1) < cut(2,2)
         'case 1'
-        childA = parentA;
-        childA(cut(1,1):cut(1,2), cut(2,1):cut(2,2)) = parentB(cut(1,1):cut(1,2), cut(2,1):cut(2,2));
-        childB = parentB;
-        childB(cut(1,1):cut(1,2), cut(2,1):cut(2,2)) = parentA(cut(1,1):cut(1,2), cut(2,1):cut(2,2));
+        childC = parentA;
+        childC(cut(1,1):cut(1,2), cut(2,1):cut(2,2)) = parentB(cut(1,1):cut(1,2), cut(2,1):cut(2,2));
+        childD = parentB;
+        childD(cut(1,1):cut(1,2), cut(2,1):cut(2,2)) = parentA(cut(1,1):cut(1,2), cut(2,1):cut(2,2));
     else
         'case 2'
-        childA = parentA;
-        childA(cut(1,1):cut(1,2), 1:cut(2,2)) = parentB(cut(1,1):cut(1,2), 1:cut(2,2));
-        childA(cut(1,1):cut(1,2), cut(2,1):end) = parentB(cut(1,1):cut(1,2), cut(2,1):end);
-        childB = parentB;
-        childB(cut(1,1):cut(1,2), 1:cut(2,2)) = parentA(cut(1,1):cut(1,2), 1:cut(2,2));
-        childB(cut(1,1):cut(1,2), cut(2,1):end) = parentA(cut(1,1):cut(1,2), cut(2,1):end);
+        childC = parentA;
+        childC(cut(1,1):cut(1,2), 1:cut(2,2)) = parentB(cut(1,1):cut(1,2), 1:cut(2,2));
+        childC(cut(1,1):cut(1,2), cut(2,1):end) = parentB(cut(1,1):cut(1,2), cut(2,1):end);
+        childD = parentB;
+        childD(cut(1,1):cut(1,2), 1:cut(2,2)) = parentA(cut(1,1):cut(1,2), 1:cut(2,2));
+        childD(cut(1,1):cut(1,2), cut(2,1):end) = parentA(cut(1,1):cut(1,2), cut(2,1):end);
     end
 else
     if cut(2,1) < cut(2,2)
         'case 3'
-        childA = parentA;
-        childA(1:cut(1,2), cut(2,1):cut(2,2)) = parentB(1:cut(1,2), cut(2,1):cut(2,2));
-        childA(cut(1,1):end, cut(2,1):cut(2,2)) = parentB(cut(1,1):end, cut(2,1):cut(2,2));
-        childB = parentB;
-        childB(1:cut(1,2), cut(2,1):cut(2,2)) = parentA(1:cut(1,2), cut(2,1):cut(2,2));
-        childB(cut(1,1):end, cut(2,1):cut(2,2)) = parentA(cut(1,1):end, cut(2,1):cut(2,2));
+        childC = parentA;
+        childC(1:cut(1,2), cut(2,1):cut(2,2)) = parentB(1:cut(1,2), cut(2,1):cut(2,2));
+        childC(cut(1,1):end, cut(2,1):cut(2,2)) = parentB(cut(1,1):end, cut(2,1):cut(2,2));
+        childD = parentB;
+        childD(1:cut(1,2), cut(2,1):cut(2,2)) = parentA(1:cut(1,2), cut(2,1):cut(2,2));
+        childD(cut(1,1):end, cut(2,1):cut(2,2)) = parentA(cut(1,1):end, cut(2,1):cut(2,2));
     else
         'case 4'
-        childA = parentA;
-        childA(1:cut(1,2), 1:cut(2,2)) = parentB(1:cut(1,2), 1:cut(2,2));
-        childA(cut(1,1):end, cut(2,1):end) = parentB(cut(1,1):end, cut(2,1):end);
-        childA(cut(1,1):end, 1:cut(2,2)) = parentB(cut(1,1):end, 1:cut(2,2));
-        childA(1:cut(1,2), cut(2,1):end) = parentB(1:cut(1,2), cut(2,1):end);
-        childB = parentB;
-        childB(1:cut(1,2), 1:cut(2,2)) = parentA(1:cut(1,2), 1:cut(2,2));
-        childB(cut(1,1):end, cut(2,1):end) = parentA(cut(1,1):end, cut(2,1):end);
-        childB(cut(1,1):end, 1:cut(2,2)) = parentA(cut(1,1):end, 1:cut(2,2));
-        childB(1:cut(1,2), cut(2,1):end) = parentA(1:cut(1,2), cut(2,1):end);
+        childC = parentA;
+        childC(1:cut(1,2), 1:cut(2,2)) = parentB(1:cut(1,2), 1:cut(2,2));
+        childC(cut(1,1):end, cut(2,1):end) = parentB(cut(1,1):end, cut(2,1):end);
+        childC(cut(1,1):end, 1:cut(2,2)) = parentB(cut(1,1):end, 1:cut(2,2));
+        childC(1:cut(1,2), cut(2,1):end) = parentB(1:cut(1,2), cut(2,1):end);
+        childD = parentB;
+        childD(1:cut(1,2), 1:cut(2,2)) = parentA(1:cut(1,2), 1:cut(2,2));
+        childD(cut(1,1):end, cut(2,1):end) = parentA(cut(1,1):end, cut(2,1):end);
+        childD(cut(1,1):end, 1:cut(2,2)) = parentA(cut(1,1):end, 1:cut(2,2));
+        childD(1:cut(1,2), cut(2,1):end) = parentA(1:cut(1,2), cut(2,1):end);
     end
+end
+
+if sum(sum(parentA==childC))>23
+    childA = childC;
+    childB = childD;
+else
+    childA = childD;
+    childB = childC;
+end
 
 end
