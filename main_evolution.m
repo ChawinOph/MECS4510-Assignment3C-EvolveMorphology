9831b7dc130dfa5501c1dc7cdbc01b456aa0c17f
Rosie <rcm2146@columbia.edu>
2018-12-08 18:14:08 -0500

    add randos into each generation

--- main_evolution.m
+++ main_evolution.m
@@ -3,30 +3,32 @@ clc
 clear
 close all
 %%
-p = 50; % Population size
-g = 10; % number of generations
-s = 0.5; % 
-m = 0.02;
-r = 0.2
+p = 10; % Population size
+g = 1; % number of generations
+s = 0.5; % selection pressure
+m = 0.1; % proportion of children that get mutated
+r = 0.2; % proportion of random individuals added to the population every gen
 %%
 genes= rand(5,9,p);
 bots = MorphCube(genes);
 sim = Simulator();
 par_layers = zeros(s*p, s*p, g);
 children = zeros(5,9,(1-s)*p);
-evaluate(sim, bots);
+fits = evaluate(sim, bots);
 
 %%
+tic
 for i = 1:g
     % Evaluate
     
-    fits = bots.fitness;
+%     fits = bots.fitness;
     ages = bots.age;
     
     % Select
     [front, idx] = pareto_pick(s-0.5*r, fits, ages);
     parent_bots = bots(idx);
     parents = parents_bots.gene;
+    par_fits = fits(idx);
     
     %Record
     par_layers(:,:,i) = front;
@@ -43,15 +45,19 @@ for i = 1:g
     chidren_bots = MorphCube(children, parent_bots.ages + 1);
     
     %Add new random individuals
-    rand_bots = MorphCube(
+    rand_bots = MorphCube(rand(5,9,r*p));
     
-    evaluate(sim, children_bots);
-    bots = [parent_bots, children_bots];
+    child_fits = evaluate(sim, children_bots);
+    rand_fits = evaluate(sim, rand_bots);
+    bots = [parent_bots, children_bots, rand_bots];
+    fits = [par_fits, child_fits, rand_fits];
     shuffle_ind = randperm(length(bots));
     bots = bots(shuffle_ind);
+    fits = fits(shuffle_ind);
 end
-
+toc
 %%
+disp('Done!!');
 plot(par_layers(1,:,:))
 
 bot_no = 17;
